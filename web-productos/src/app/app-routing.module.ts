bc9cd97fdbcf75666a52aa33dad9d3192956698a
Jose Angel <angel251095@gmail.com>
2019-05-18 20:18:46 -0600

    API y vista de productos

---
+++ web-productos/src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {ProductEditComponent} from './product-edit/product-edit.component';
+import {ProductAddComponent} from './product-add/product-add.component';
+
+const routes: Routes = [{
+    path:'products',
+    component: ProductsComponent,
+    data: {title:'Lista de productos'}
+  },
+  {
+    path:'product-details/:id',
+    component: ProductDetailComponent,
+    data:{title:'Detalle de producto'}
+  },
+  {
+    path: 'product-add',
+    component: ProductAddComponent,
+    data:{title:'Agregar nuevo producto'},
+  },
+  {
+    path: 'product-edit/:id',
+    component: ProductEditComponent,
+    data: {title: 'Editar datos de producto'}
+  },
+  {
+    path:'',
+    redirectTo:'/product',
+    pathMatch:'full'
+  }
+ ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
