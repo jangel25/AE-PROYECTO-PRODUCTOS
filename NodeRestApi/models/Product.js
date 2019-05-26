3689e1b753ceee7c507348c866499c3b5dea5fa9
Luz Gonzalez <luzalgonzalezro@ittepic.edu.mx>
2019-05-26 12:14:00 -0600

    feat:add campo prod_img a Product.js y a Product.ts y add nuevo campo al formulario de la vista html product-edit

--- NodeRestApi/models/Product.js
+++ NodeRestApi/models/Product.js
@@ -4,6 +4,7 @@ var ProductSchema = new mongoose.Schema({
   prod_name: String,
   prod_desc: String,
   prod_price: Number,
+  prod_img: String,
   updated_at: { type: Date, default: Date.now },
 });
 
